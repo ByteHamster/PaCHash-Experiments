9e5380f3e0c2e6580a90dc3beae5a1e9544e5e29
ByteHamster <info@bytehamster.com>
2021-09-13 18:06:53 +0200

    Tiny fixes

--- rocksdb.cpp
+++ rocksdb.cpp
@@ -19,7 +19,6 @@ static std::vector<uint64_t> generateRandomKeys(size_t N) {
 }
 
 void testRocksDb() {
-    rocksdb::DB* db;
     rocksdb::Options options;
     options.create_if_missing = true;
     rocksdb::BlockBasedTableOptions table_options;
@@ -27,27 +26,35 @@ void testRocksDb() {
     options.table_factory.reset(NewBlockBasedTableFactory(table_options));
     options.use_direct_reads = true;
     options.allow_mmap_reads = false;
+    options.env = rocksdb::Env::Default();
     std::string filePath = "/home/hans-peter/testdb";
+
+    std::vector<rocksdb::ColumnFamilyDescriptor> x;
+    rocksdb::DestroyDB(filePath, options, x);
+
+    rocksdb::DB* db;
     rocksdb::Status status = rocksdb::DB::Open(options, filePath, &db);
     if (!status.ok()) {
         std::cout<<status.ToString()<<std::endl;
         return;
     }
 
-    size_t numKeys = 500;
+    size_t numKeys = 1e5;
     std::vector<std::uint64_t> keys = generateRandomKeys(numKeys);
     RandomObjectProvider randomObjectProvider(EQUAL_DISTRIBUTION, 512);
     std::cout<<"Inserting"<<std::endl;
     rocksdb::WriteOptions writeOptions;
-    writeOptions.sync = true;
+    writeOptions.disableWAL = true;
     for (uint64_t keyUint : keys) {
         rocksdb::Slice key(reinterpret_cast<const char *>(&keyUint), sizeof(uint64_t));
         size_t length = randomObjectProvider.getLength(keyUint);
         const char *value = randomObjectProvider.getValue(keyUint);
         db->Put(writeOptions, key, rocksdb::Slice(value, length));
     }
+    std::cout<<"Flushing"<<std::endl;
+    db->Flush(rocksdb::FlushOptions());
 
-    size_t numBatches = 8000;
+    size_t numBatches = 4000;
     size_t batchSize = 20;
     std::cout<<"Querying"<<std::endl;
     auto queryStart = std::chrono::high_resolution_clock::now();
@@ -62,9 +69,6 @@ void testRocksDb() {
             querySlices.at(k) = rocksdb::Slice(reinterpret_cast<const char *>(queryKeys.data() + k), sizeof(uint64_t));
         }
         std::vector<rocksdb::Status> status = db->MultiGet(rocksdb::ReadOptions(), querySlices, &values);
-        for (int k = 0; k < 10; k++) {
-            db->Get(rocksdb::ReadOptions(), querySlices.at(k), &values.at(k));
-        }
 
         for (int k = 0; k < batchSize; k++) {
             //uint64_t keyUint = queryKeys.at(k);
@@ -85,6 +89,4 @@ void testRocksDb() {
     std::cout<<"kQueries/s: "<<(double)numBatches*(double)batchSize*1000.0/((double)timeMicroseconds)<<std::endl;
 
     db->Close();
-    std::vector<rocksdb::ColumnFamilyDescriptor> x;
-    rocksdb::DestroyDB(filePath, options, x);
 }

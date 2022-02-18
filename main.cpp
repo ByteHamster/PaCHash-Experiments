a0d69f88f6984c2e0ea7a525b2a79dfdc1ef03f9
ByteHamster <info@bytehamster.com>
2022-02-18 11:45:48 +0100

    Test more interesting ranges

--- main.cpp
+++ main.cpp
@@ -5,26 +5,27 @@
 #include "test_pachash.h"
 
 int main() {
-    for (size_t i = 1; i <= 12; i++) {
-        size_t N = i * 1e6;
-        size_t averageLength = 256;
-        size_t numQueries = 1e6;
+    for (size_t N = 1e5; N <= 3e6; N += 1e5) {
+        for (size_t i = 0; i < 3; i++) {
+            size_t averageLength = 256;
+            size_t numQueries = 1e6;
 
-        // Full data store
-        SiltComparisonItem(N, averageLength, numQueries).performBenchmark();
-        LevelDBComparisonItem(N, averageLength, numQueries).performBenchmark();
-        RocksDBComparisonItem(N, averageLength, numQueries).performBenchmark();
-        PaCHashComparisonItem(N, averageLength, numQueries).performBenchmark();
+            // Full data store
+            SiltComparisonItem(N, averageLength, numQueries).performBenchmark();
+            LevelDBComparisonItem(N, averageLength, numQueries).performBenchmark();
+            RocksDBComparisonItem(N, averageLength, numQueries).performBenchmark();
+            PaCHashComparisonItem(N, averageLength, numQueries).performBenchmark();
 
-        // Partial benchmark
-        LevelDBSingleTableComparisonItem(N, averageLength, numQueries).performBenchmark();
-        SiltComparisonItemSortedStore(N, averageLength, numQueries).performBenchmark();
+            // Partial benchmark
+            LevelDBSingleTableComparisonItem(N, averageLength, numQueries).performBenchmark();
+            SiltComparisonItemSortedStore(N, averageLength, numQueries).performBenchmark();
 
-        // Microbenchmark
-        SiltComparisonItemSortedStoreMicro(N, averageLength, numQueries).performBenchmark();
-        numQueries = 5e6;
-        PaCHashMicroIndexComparisonItem(N, averageLength, numQueries).performBenchmark();
-        LevelDBSingleTableMicroIndexComparisonItem(N, averageLength, numQueries).performBenchmark();
+            // Microbenchmark
+            SiltComparisonItemSortedStoreMicro(N, averageLength, numQueries).performBenchmark();
+            numQueries = 5e6;
+            PaCHashMicroIndexComparisonItem(N, averageLength, numQueries).performBenchmark();
+            LevelDBSingleTableMicroIndexComparisonItem(N, averageLength, numQueries).performBenchmark();
+        }
     }
     return 0;
 }

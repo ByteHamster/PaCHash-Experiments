8e27e1e0bea176ce41807a4461d0ff4d47d8beae
ByteHamster <info@bytehamster.com>
2022-04-08 14:29:26 +0200

    Store only hash in unordered_map

--- test_std_unordered_map.h
+++ test_std_unordered_map.h
@@ -2,7 +2,7 @@
 
 class StdUnorderedMapComparisonItem : public StoreComparisonItem {
     public:
-        std::unordered_map<std::string, size_t> map;
+        std::unordered_map<uint64_t, size_t> map;
 
         StdUnorderedMapComparisonItem(size_t N, size_t objectSize, size_t numQueries) :
                 StoreComparisonItem("unordered_map", N, objectSize, numQueries) {
@@ -15,13 +15,13 @@ class StdUnorderedMapComparisonItem : public StoreComparisonItem {
         void construct(std::vector<std::string> &keys) override {
             map.reserve(keys.size());
             for (std::string &key : keys) {
-                map.insert(std::make_pair(key, 42));
+                map.insert(std::make_pair(pachash::MurmurHash64(key), 42));
             }
         }
 
         void query(std::vector<std::string> &keysQueryOrder) override {
             for (size_t i = 0; i < numQueries; i++) {
-                size_t result = map.at(keysQueryOrder[i]);
+                size_t result = map.at(pachash::MurmurHash64(keysQueryOrder[i]));
                 DO_NOT_OPTIMIZE(result);
             }
         }

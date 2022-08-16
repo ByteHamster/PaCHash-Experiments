f9b5846392a7083c95ce0e3a031e437fef4bc721
ByteHamster <info@bytehamster.com>
2022-08-16 18:53:34 +0200

    Allow applying patches outside a git repo

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -5,9 +5,13 @@ for d in * ; do
     cd $d
     patchfile="../../patches/$d.patch"
     if [[ -f "$patchfile" ]]; then
-        git restore --staged .
-        git restore .
+         # If not a git repo, applying the patch is enough, so we ignore errors here
+        git restore --staged . 2>/dev/null
+        git restore . 2>/dev/null
+
         git apply $patchfile
     fi
     cd ..
 done
+echo "If you don't see errors above, the patches have been applied successfully."
+

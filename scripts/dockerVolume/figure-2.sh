f59324142786fa225010a9b9f036061b8841eacc
ByteHamster <info@bytehamster.com>
2022-08-17 11:10:07 +0200

    Improve docker scripts

--- scripts/dockerVolume/figure-2.sh
+++ scripts/dockerVolume/figure-2.sh
@@ -2,7 +2,8 @@
 
 # Run benchmark
 cd /opt/pachash/external/pachash/build
-params="--uring_io --store_file /opt/testDirectory --num_queries 100k --iterations 1 --queue_depth 128 --object_size_distribution normal"
+rm -f figure-2.txt
+params="--uring_io --store_file /opt/testDirectory/filename.dat --num_queries 3M --iterations 1 --queue_depth 128 --object_size_distribution normal"
 for objectSize in $(seq 128 128 896); do
     numObjects=$((1500000000/$objectSize)) # Always 1.5 GB
     ./Benchmark --pachash 2 --load_factor 1 $params --object_size $objectSize --num_objects $numObjects | tee --append figure-2.txt
@@ -13,8 +14,9 @@ done
 
 # Build plot
 cd /opt/pachash/scripts
-cp /opt/pachash/build/figure-2.txt figure-2.txt
+cp /opt/pachash/external/pachash/build/figure-2.txt figure-2.txt
 /opt/sqlplot-tools/build/src/sqlplot-tools figure-2.tex
+pdflatex figure-2.tex
 pdflatex figure-2.tex
 cp figure-2.pdf /opt/dockerVolume
 cp figure-2.txt /opt/dockerVolume
--- scripts/dockerVolume/figure-4.sh
+++ scripts/dockerVolume/figure-4.sh
@@ -2,12 +2,13 @@
 
 # Run benchmark
 cd /opt/pachash/build
-./ObjectStoreComparison --path /opt/testDirectory --delta_step 1M --repetitions 1 | tee figure-4.txt
+./ObjectStoreComparison --path /opt/testDirectory --delta_step 1500k --repetitions 1 | tee figure-4.txt
 
 # Build plot
 cd /opt/pachash/scripts
 cp /opt/pachash/build/figure-4.txt figure-4.txt
 /opt/sqlplot-tools/build/src/sqlplot-tools figure-4.tex
+pdflatex figure-4.tex
 pdflatex figure-4.tex
 cp figure-4.pdf /opt/dockerVolume
 cp figure-4.txt /opt/dockerVolume
